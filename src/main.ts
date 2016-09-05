61f1e4ba8c8d3e942b371ba9e225438885c8577f
Sandeep Perkari <sandeep.perkari@gmail.com>
2016-09-05 13:41:23 -0400

    commit

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
 import { APP_ROUTER_PROVIDERS } from './app/app.routing';
+import {HTTP_PROVIDERS} from '@angular/http';
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent,[APP_ROUTER_PROVIDERS]);
+bootstrap(AppComponent,[APP_ROUTER_PROVIDERS,HTTP_PROVIDERS]);
