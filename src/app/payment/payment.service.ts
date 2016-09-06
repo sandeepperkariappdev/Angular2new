167829a7ddb5d8113c281c93a82c23b0bd0f2175
Sandeep Perkari <sandeep.perkari@gmail.com>
2016-09-06 18:35:25 -0400

    cards and claims alignment commit

--- src/app/payment/payment.service.ts
+++ src/app/payment/payment.service.ts
@@ -5,27 +5,12 @@ import {Hero} from './Hero';
 
 @Injectable()
 export class PaymentService {     
-    payments:Array<any>;
+    
     constructor(private http:Http) {
-        this.payments = [
-            { age: 40, name: 'Jordan Houston' },
-            { age: 23, name: 'Josh Beh' },
-            { age: 23, name: 'Joseph Canina' },
-            { age: 24, name: 'Alexandra Wilkins' },
-            { age: 22, name: 'Kiersten Costanzo' },
-            { age: 23, name: 'Ku Sherwood' },
-            { age: 25, name: 'Arta Halili' },
-            { age: 21, name: 'Patrick Cooney' },
-            { age: 21, name: 'Z.A. Perr' },
-            { age: 18, name: 'Tyler Mulligan' },
-            { age: 26, name: 'Dennis Dempsey' },
-            { age: 32, name: 'Francis Yeager' },
-            { age: 23, name: 'Phil Belardi' },
-            { age: 25, name: 'Bryan Roman' }
-        ];
+       
     }
     getPayments():Observable<Hero[]> {
-     //   return this.payments;
+     
       return this.http.get('app/heroes.json').map(this.extractData).catch(this.handleError);
     }
      private extractData(res:Response){
