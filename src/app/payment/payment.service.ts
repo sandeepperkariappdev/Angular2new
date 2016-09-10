b546d2e66a3918394bc65dd5a90eefb4d27b49ff
Sandeep Perkari <sandeep.perkari@gmail.com>
2016-09-10 07:30:00 -0400

    payments

--- src/app/payment/payment.service.ts
+++ src/app/payment/payment.service.ts
@@ -2,19 +2,23 @@ import { Injectable } from '@angular/core';
 import { Http, Response,Headers, RequestOptions } from '@angular/http';
 import {Observable} from 'rxjs/Rx';
 import {Hero} from './Hero';
-
+const url =   "https://airporthubops-qa-enfsvc.ual.com/sdb/SDBAPI/Channels/HTTP/ArptSuprView?versionNo=2.0&gates=C-30,C-31,C-32,C-33,C-34,C-35,C-36,C-37,C-39,C-40,C-41,C-42,C-43,C-44,C-45,E1,E2,E3,E4,E5,E7,E8,E10,E11,E12,E14,E15,E16,E9,E17,E18,E19,E20,E21,E22,E23,E24,C-14,C-16,C-17,C-18,C-19,C-20,C-21,C-22,C-23,C-24,C-25,C-26,D1,D10,D11,D12,D2,D3,D4,D4A,D5,D6,D6A,D7,D8,D9,A1,A2,A7,A8,B76,B77,B79,B79A,B80,B81,B83,B85A,B86,B87,B88,B1,B10,B11,B12,B14,B15,B16,B2,B3,B4,B5,B6,B7,B8,B9,B17,B18,B19,B20,B21,B22,B23,B24,B25,B26,B27,B29,B30,B31&rplyInJson=true&station=IAH&viewType=ALL";
+const token = "Bearer AAENU1ZDLVRTVkRCT0FSRIgt4UXY0D_xXLOqhcYb5-Ak5C1U4GiN52SOzsGNHdzkp7JxBfwV6m5QOw46nLKxMcIgzwcaTnUbMbCL9ugz0Xg";
 @Injectable()
 export class PaymentService {     
     
     constructor(private http:Http) {
        
     }
-    getPayments():Observable<Hero[]> {
-     
-      return this.http.get('app/heroes.json').map(this.extractData).catch(this.handleError);
+
+      getPayments():Observable<Hero[]> {
+       let headers = new Headers({ 'Content-Type': 'application/json','Authorization':token });      
+        let options = new RequestOptions({ headers: headers });
+      return this.http.get(url,options).map(this.extractData).catch(this.handleError);
     }
      private extractData(res:Response){
         let body = res.json();
+        console.log(body);
         return body.data || {};
     }
     private handleError (error: any) {
